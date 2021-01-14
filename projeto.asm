83745593599a41be40a596ee054faffc248c05dc
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2021-01-13 21:12:21 -0300

    feat: distributionOfPieces iterator

--- projeto.asm
+++ projeto.asm
@@ -1,36 +1,39 @@
 # table of responsibilities of registrators 
 # $s0 -> flag of gameloop
 # $s1 -> aux of small works
+# $s7 -> aux for player iterator
 
 .data
-hexdigits: .asciiz "1|2", "0|1", "0|2", "0|3", "0|3", "0|4", "0|5", "0|6", "1|0", "1|1", "1|2", "1|3", "1|3", "1|4", "1|5", "1|6","2|0", "2|1", "2|2", "2|3", "2|3", "2|4", "2|5", "2|6","3|0", "3|1", "3|2", "3|3", "3|3", "3|4", "3|5", "3|6","4|0", "4|1", "4|2", "4|3", "4|3", "4|4", "4|5", "4|6","5|0", "5|1", "5|2", "5|3", "5|3", "5|4", "5|5", "5|6","6|0", "6|1", "6|2", "6|3", "6|3", "6|4", "65|5","6|6"
-teste: .asciiz "seila"
-jogador1: .word 0 0 0 0 0 0 0
-.text
-la $a0, hexdigits+3 # 1|2 -    +2 = 2   +1 = |2  sem nada = 1|2 
-li $v0, 4
+pieces: .word 0, 1, 2, 3, 4, 5, 6, 11, 12, 13, 14, 15, 16, 22, 23, 24, 25, 26, 33, 34, 35, 36, 43, 44, 45, 46, 55, 56, 66
+jogador1: .word -1 -1 -1 -1 -1 -1 -1
+jogador2: .word -1 -1 -1 -1 -1 -1 -1
+jogador3: .word -1 -1 -1 -1 -1 -1 -1
+jogador4: .word -1 -1 -1 -1 -1 -1 -1
 
-syscall
+.text
+
 # Tabuleiro: 1|2 => 2|5 => 1|2 => 2|5 
 
 addi $s0, $zero, 1 # flag for gameLoop ($s0 = 1)
 
 gameLoop: beq $s0, 0, gameLoopEnd # $s0 == 0 then go to gameLoopEnd
-# addi $s1, $zero, 7 # flag 7 for while of distributionOfPieces
-distributionOfPieces:
+
+distributionOfPieces: beq $s1,28, distributionOfPiecesEnd # while($s1 != 28)
+	# distribution address of pieces for players...
+	addi $s1, $s1, 1 # $s1 += 1
+	j distributionOfPieces # back to distributionOfPieces
 distributionOfPiecesEnd:
-	j gameLoop # back to 'gameLoop'
-# 25 % 10
 
 # jogador X 
-# peça escolhilda é valida ? Se for diferente de jogador 1 verificar todas peças  
-# joga a peça
+# peÃ§a escolhilda Ã© valida ? Se for diferente de jogador 1 verificar todas peÃ§as  
+# joga a peÃ§a
 # printa o tabuleiro
-#limpa a peça de jogadorX
+#limpa a peÃ§a de jogadorX
 
-# if Jogador X tem zero peças ? 
+# if Jogador X tem zero peÃ§as ? 
 # if se o jogo fechou  
 # jogador X + 1
 
+	j gameLoop # back to 'gameLoop'
 gameLoopEnd: 	addi $v0, $zero, 10 #syscal of end program
 		syscall
