62d28e9a8ac5f107c36c0f92527e4b009ac1deaa
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2021-02-07 15:24:08 -0300

    feat: ensure play piece in board

--- projeto2.asm
+++ projeto2.asm
@@ -1,5 +1,7 @@
 # $s0 -> gameloop
 # $s5 -> player iterator
+# $s3 -> lastpiece board
+# $t1 -> piece that will be played 
 
 .data
 pieces: .word 0, 1, 2, 3, 4, 5, 6, 11, 12, 13, 14, 15, 16, 22, 23, 24, 25, 26, 33, 34, 35, 36, 43, 44, 45, 46, 55, 56, 66
@@ -9,10 +11,9 @@ jogador3: .word -2, -1, -1, -1, -1, -1, -1	# save pieces
 jogador4: .word -3, -1, -1, -1, -1, -1, -1	# save pieces
 board:    .word 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99 
 
-data_jogadorForOut: .asciiz "-1 -1 -1 -1 -1 -1 -1"
+data_jogadorForOut: .asciiz "-1 -1 -1 -1 -1 -1 -1 \0"
 
-prompt_selectPiece: .asciiz "Select piece: (0-6)"
-reply_prompt_pieceNumber: .asciiz "00" # including '\0'
+prompt_selectPiece: .asciiz "*Select piece: (0-6)"
 
 round_info_prompt_str: .asciiz "* Round: " 		# 9 positions
 player_info_prompt_str: .asciiz "-*Player: " 		# 10 positions
@@ -20,7 +21,6 @@ var_number_info_str: 	.asciiz "000"			# including '\0'
 
 player_pieces_prompt_str: .asciiz "--*Player Pieces: "
 
-ln_str: .asciiz ""			# it's '\n'
 
 
 .text
@@ -57,6 +57,7 @@ j distributionOfPieces 					# back to distributionOfPieces
 distributionOfPiecesEnd:
 
 ## ====== GAMELOOP ====== ##
+addi $s5, $zero, 0 # $s5 = 0
 addi $s0, $zero, 1 # flag for gameLoop ($s0 = 1)
 gameLoop: beq $s0, 0, gameLoopEnd # $s0 == 0 then go to gameLoopEnd
 
@@ -65,6 +66,43 @@ jal print_round_info
 jal print_player_pieces_info
 
 
+#bne $s5, 0, inputPieceOfPlayer1End      # if (s5 != 0) jump to inputPieceOfPlayer1End
+#verify if have possible piece
+addi $s1, $zero, 0 	# $s1 set to zero
+addi $t7, $zero, 0 	# $t7 set to zero
+# has_possible_pieces: slti $t0, $s1, 7			# for i=0;i<7;i++ 
+# 		beq $t0, $zero, botChoicePieceEnd 	# $t0 == 0 end loop
+		
+# 		jal pieceVerification			# ($s1 with param)(return => $t1 for piece selected, rerturn => $t7 result of verification)
+# 		bne $t7, 0, has_possible_pieces_End
+			
+# 		addi $s1, $s1, 1			# $s1 += 1
+# 		j has_possible_pieces			# back loop
+# has_possible_pieces_End:
+
+#beq $t7, 0, pre_round_end		# if not has possible_piece, go to next round
+
+inputPieceOfPlayer1:
+# Print prompt
+	la $a0, prompt_selectPiece # address of string to print
+	li $v0, 4
+	syscall
+
+# Input piece
+	li $v0, 5
+	syscall		#return int on $v0
+	
+	add $t1, $zero,$v0		# $t1 = $v0
+	
+	#jal pieceVerification			# ($t1 for piece selected, $t7 result of verification)
+	#bne $t7, 0, botChoicePieceOkay		# "botChoicePieceOkay" is a point where the piece is valid, and $ s2 is set to 1, (so there is a piece available in the round)
+	#j inputPieceOfPlayer1
+inputPieceOfPlayer1End:
+
+# $t1 save piece choice in $t1
+#TEST
+addi $t7, $zero, 2
+jal play_piece_in_board		# need($t7)
 
 pre_round_end:
 
@@ -152,6 +190,145 @@ set_player_loop_piece_end:
 
 jr $ra		# End rotine
 
+################################################################################################
+pieceVerification: # ($s1 with param)(return => $t1 for piece selected, rerturn => $t7 result of verification) 
+										# if $t7 == 1, first place of board is valid, if t7 == 2 last place of board is valid
+addi $t1, $s1, 0				# $t1 = $s1(iteretor for on botChoicePiece)
+
+	mul  $t1, $t1, 4 			# $t1 = position in bytes of pieces in jogador .word
+	
+bne $s5, 0, verifyPiecePlayer1End		# if ($s5 != 0)
+verifyPiecePlayer1:
+	lw   $t2, jogador1($t1)			# $t2 = jogador[$t1]				
+verifyPiecePlayer1End:			
+
+bne $s5, 1, verifyPiecePlayer2End		# if ($s5 != 1)
+verifyPiecePlayer2:
+	lw   $t2, jogador2($t1)			# $t2 = jogador[$t1]
+verifyPiecePlayer2End:
+
+bne $s5, 2, verifyPiecePlayer3End		# if ($s5 != 2)
+verifyPiecePlayer3:
+	lw   $t2, jogador3($t1)			# $t2 = jogador[$t1]
+verifyPiecePlayer3End:
+
+bne $s5, 3, verifyPiecePlayer4End		# if ($s5 != 3)
+verifyPiecePlayer4:
+	lw   $t2, jogador4($t1)			# $t2 = jogador[$t1]
+verifyPiecePlayer4End:
+	  
+	# player's piece
+	li   $t5, 10				# $t5 = 10
+	div  $t2, $t5		
+	mfhi $t2				# $t2 = 1st number # ex: if number is 65, hi = 6 
+	mflo $t3				# $t3 = 2nd number
+	
+	# validate first round six's bomb
+	bne $s0, 1, is_not_first_Round
+	bne $t2, 6, invalid_first_piece		# first part of player's piece is diff of '6'
+	bne $t3, 6, invalid_first_piece		# first part of player's piece is diff of '6'
+	j pieceVerificationEndOkay2		# this piece is '66'
+	is_not_first_Round:
+	
+	#first piece of board
+	lw   $t4, board($zero)			# $t4 = board[0]
+	
+	div  $t4, $t5				# $t4 / ($t5 = 10)
+	mfhi $t4					# $t4 = 1st number
+	
+	beq  $t2, $t4, pieceVerificationEndOkay1 # if $t2 == $t4 is valid
+	beq  $t3, $t4, pieceVerificationEndOkay1 # if $t2 == $t4 is valid
+	
+	#last piece of board
+	mul  $t6, $s3, 4			# $s3 -> last piece of board (position)
+	lw   $t4, board($t6)			# $t4 = board[0]
+	
+	beq $t4, -1, pieceVerificationEndOkay2	# if last piece of board equal -1, so not have pieces in board
+	
+	div  $t4, $t5				# $t4 / ($t5 = 10)
+	mflo $t4				# $t4 = 2nd number
+	
+	beq  $t2, $t4, pieceVerificationEndOkay2 # if $t2 == $t4 is valid
+	beq  $t3, $t4, pieceVerificationEndOkay2 # if $t2 == $t4 is valid
+	
+	addi $t7, $zero, 0	# $t7 = 0
+	j pieceVerificationEnd	# go to end rotine
+	
+pieceVerificationEndOkay1:
+	addi $t7, $zero, 1	# $t7 = 1
+	j pieceVerificationEnd
+pieceVerificationEndOkay2:
+	addi $t7, $zero, 2	# $t7 = 1
+pieceVerificationEnd:
+jr $ra		# End rotine
+
+invalid_first_piece:
+addi $s5, $s5, 1
+j gameLoop
+
+###############################################################################################
+
+play_piece_in_board:	#($t1 for piece index) 
+# if $t7 == 1, first place of board is valid, if t7 == 2 last place of board is valid
+mul  $t1, $t1, 4 			# $t1 = position in bytes of pieces in jogador .word
+
+li  $t8, 99							# charge $t8 with number that represents null in logic game
+
+bne $s5, 0, setPiecePlayer1End		# if ($s5 != 0)
+setPiecePlayer1:
+	lw   $t2, jogador1($t1)			# $t2 = jogador[$t1]
+	sw	 $t8, jogador1($t1) 		# jogador[$t1] = 99
+setPiecePlayer1End:			
+
+bne $s5, 1, setPiecePlayer2End		# if ($s5 != 1)
+setPiecePlayer2:
+	lw   $t2, jogador2($t1)			# $t2 = jogador[$t1]
+	sw	 $t8, jogador2($t1) 		# jogador[$t1] = 99
+setPiecePlayer2End:
+
+bne $s5, 2, setPiecePlayer3End		# if ($s5 != 2)
+setPiecePlayer3:
+	lw   $t2, jogador3($t1)			# $t2 = jogador[$t1]
+	sw	 $t8, jogador3($t1) 		# jogador[$t1] = 99
+setPiecePlayer3End:
+
+bne $s5, 3, setPiecePlayer4End		# if ($s5 != 3)
+setPiecePlayer4:
+	lw   $t2, jogador4($t1)			# $t2 = jogador[$t1]
+	sw	 $t8, jogador4($t1) 		# jogador[$t1] = 99
+setPiecePlayer4End:
+
+beq $t7, 1, play_first_place_board
+beq $t7, 2, play_last_place_board
+
+play_first_place_board:
+# for (int i = 0; i<($s3+1)) # precisa deslocar todas pe�as uma casa pra frente
+add $s1, $zero, $s3
+add $t0, $zero, $zero		 # $t0 = 0
+reposition_board_loop: beq $t0, $s1, reposition_board_loop_end			# for i=s3;i>0+1;i--  	
+		addi $t9, $s1, 1							# $t9 = nextpostion
+
+		mul $t8, $s1, 4								# $t8 = ($s1*4)
+		lw  $s6, board($t8)							# $t6 = board[$t8]
+
+		mul  $t9, $t9, 4
+		sw $s6, board($t9)							# board[$s1+1] = board[$s1]
+		
+		subi $s1, $s1, 1							# $s1 = $s1 - 1
+		
+		j reposition_board_loop					#´back loop
+reposition_board_loop_end: 
+
+sw $t2, board($zero)								# board[0] = $t2
+
+play_last_place_board:
+mul $t8, $s3, 4		# last position number * 4
+sw $t2, board($t8)	# board[$t8] 
+
+
+play_place_board_end:
+jr $ra #end rotine
+
 ##########################################################################################
 print_player_pieces_info:	
 	# Print prompt round info
@@ -224,9 +401,3 @@ print_round_info:	# (need)$s0,$s5 | (use)$t7,$t8,$t9
 	jr $ra
 ###################################################################################
 
-print_nextline:
-	la $a0, ln_str  # address of string to print
-	li $v0, 4
-	syscall
-
-jr $ra
